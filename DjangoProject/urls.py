b3cdbaf87ac1321d8e056f06a1308d1e2898e57f
matejko4 <148313944+matejko4@users.noreply.github.com>
2026-05-09 13:14:30 +0200

    Clean up urls.py by removing unused imports
    Removed unnecessary imports and simplified urlpatterns.

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('', lambda request: redirect('seznam_hracu')),
-path('', include('seznamHracu.urls')),
+
 ]
